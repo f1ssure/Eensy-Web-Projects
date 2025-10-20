a44638f1983849ead6e42940e9ee4d6833a15490
Gabrielius Bakas <gabrielius.bakas@mif.stud.vu.lt>
2025-10-20 17:27:17 +0300

    defined "types" area in HTML & CSS, queried more elements in JS, built initial creature list fetching logic

--- RPG Creature Search App/script.js
+++ RPG Creature Search App/script.js
@@ -1,3 +1,37 @@
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const creatureName = document.getElementById('creature-name');
+const creatureId = document.getElementById('creature-id');
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
+const types = document.getElementById('types');
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const specialAttack = document.getElementById('special-attack');
+const specialDefense = document.getElementById('special-defense');
+const speed = document.getElementById('speed');
+
+const creatureListUrl = "https://rpg-creature-api.freecodecamp.rocks/api/creatures";
+const creatureDataUrl = "https://rpg-creature-api.freecodecamp.rocks/api/creature/{name-or-id}";
+
+const creatures = [];
+
+const populateCreaturesArr = (data) => {
+    data.forEach((element) => {
+        const { id, name } = element;
+        creatures.push({ id, name })
+   });
+}
+
+const fetchData = async () => {
+    try {
+        const res = await fetch(creatureListUrl);
+        const data = await res.json();
+        populateCreaturesArr(data);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+fetchData();
